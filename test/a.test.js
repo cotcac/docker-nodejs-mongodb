2556e25477c30907fbe23cef973b5e85b7cf87e9
Your Name Here <your@email.com>
2021-06-19 07:40:03 +0700

    update test

--- test/a.test.js
+++ test/a.test.js
@@ -2,6 +2,6 @@ const assert = require('chai').assert;
 const a = require('../a');
 describe('a', function(){
   it(' a should return 5', function(){
-    assert.equal(a(),4)
+    assert.equal(a(),5)
   })
 })
